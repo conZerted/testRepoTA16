348170dc726d2c1ed0ab321ade0a14ff810d9877
Joel <Joel@172.30.12.23>
2017-09-20 10:58:54 +0300

    Other update

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Siin on väljaprint");
+		System.out.println("Siin on teine väljaprint");
 
 	}
 
