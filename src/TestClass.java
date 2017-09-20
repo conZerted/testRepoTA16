c566dcdb77b90b5feb5d6d4a3f2f67db959acf7f
conZerted <pahkla.joel@gmail.com>
2017-09-20 11:07:48 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,8 +3,9 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Siin on v‰ljaprint");
-		System.out.println("Siin on teine v‰ljaprint");
+		System.out.println("Siin on v√§ljaprint");
+		System.out.println("Siin on teine v√§ljaprint");
+		System.out.println("githubist tehtud update");
 
 	}
 
